671c67d3839dc97018d902473bdca7ab20a923de
Davidson Chikezie <davidpablo007@icloud.com>
2024-05-09 09:10:52 +0100

    news

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const btnSubmit = document.querySelector(".subscribe_btn");
+const btnDismiss = document.querySelector(".btn_dismiss");
+const formContainer = document.querySelector(".form_container");
+const successEl = document.querySelector("succes_message");
+const inputEl = document.querySelector("form input");
+
+btnSubmit.addEventListener("click", () => {
+    console.log ("working");
+    if (inputEl.value === "") {
+        formContainer.classList.add("hide");
+        successEl.classList.remove("hide");
+        document.querySelector(".error p").innerHTML = "";
+    } else {
+        document.querySelector(".error p").innerHTML = "Valid Email Required";
+        inputEl.classList.add("active");
+    }
+});
+
+btnDismiss.addEventListener("click", () => {
+    formContainer.classList.remove("hide");
+    successEl.classList.add("hide");
+    inputEl.value = "";
+    inputEl.classList.remove("active");
+
+});
